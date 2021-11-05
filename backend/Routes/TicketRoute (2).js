67991d0c9eafedac5aa0d28e38821f21cb42a462
NIkhil Kumar Singh <nks130501@gmail.com>
2021-11-05 23:42:46 +0530

    Delete TicketRoute (2).js

--- backend/Routes/TicketRoute (2).js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const TicketController = require("../Controllers/TicketController");
-
-
-router.post('/', TicketController.addTicket);
-
-module.exports = router;
